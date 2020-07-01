6c3c827c09db71d8c499a1cc52db04d2fd6a912e
Sarah Greene <sgreene807@gmail.com>
2020-06-30 21:41:34 -0400

    building logic for iterations 1 and 2

---
+++ src/js/donutmaker.js
@@ -0,0 +1,56 @@
+const donut = {
+    "count": 0
+}
+
+const autoClicker = {
+    "count": 0,
+    "cost": 100
+}
+
+const donutMultiplier = {
+    "count": 0,
+    "cost": 10
+}
+
+const createDonut = () => {
+    donut.count += 1
+}
+
+const getDonutCount = () => {
+    return donut.count; 
+}
+
+const getDonutMultiplierCount = () => {
+    return donutMultiplier.count;
+}
+
+const getAutoClickerCount = () => {
+    return autoClicker.count;
+}
+
+const purchaseAutoClicker = () => {
+    while (autoClicker.count > 0){
+        if (donut.count >= autoClicker.cost){
+        donut.count - autoClicker.cost;
+        autoClicker.cost += (autoClicker.cost * .10)
+        autoClicker.count++;
+        }
+    }
+}
+
+const purchaseDonutMultiplier = () => {
+    while (donutMultiplier.count > 0){
+        if (donut.count >= donutMultiplier.cost){
+            donut.count - donutMultiplier.cost;
+            donutMultiplier.cost += (donutMultiplier.cost * .10);
+            donutMultiplier.count++;
+        }
+    }
+}
+
+
+const activateAutoClickersEvent = () => {
+    donut.count += autoClicker.count;
+}
+
+
